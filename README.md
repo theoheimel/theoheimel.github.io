aac4d4aeba592a0f413565e4e427e2dcad4675a1
Theo Heimel <info@theovention.de>
2024-12-25 16:29:39 +0100

    add cv website

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository contains the code for my personal website, [theoheimel.com](https://theoheimel.com).
+The design is based on [devfolio](https://github.com/RyanFitzgerald/devfolio), but the template was
+modified to work with Jekyll and GitHub pages.
---
+++ _update_references.py
@@ -0,0 +1,46 @@
+import json
+from urllib.request import urlopen
+
+url = "https://inspirehep.net/api/literature?sort=mostrecent&size=100&q=f+a+Heimel,+T"
+max_authors = 6
+
+with urlopen(url) as f:
+    response = json.load(f)
+
+with open("_data/publications.yaml", "w") as f:
+    publications = response["hits"]["hits"]
+    for publication in publications:
+        pub_meta = publication["metadata"]
+        title = pub_meta["titles"][0]["title"]
+        if "arxiv_eprints" not in pub_meta:
+            continue
+        arxiv_id = pub_meta["arxiv_eprints"][0]["value"]
+        if "publication_info" in pub_meta:
+            journal_info = pub_meta["publication_info"][0]
+            journal = (
+                f"{journal_info['journal_title']} {journal_info['journal_volume']}, "
+                f"{journal_info['artid']} ({journal_info['year']})"
+            )
+        else:
+            journal = None
+        for info in pub_meta.get("publication_info", []):
+            if "pubinfo_freetext" in info:
+                journal = info["pubinfo_freetext"]
+                break
+        n_authors = len(pub_meta["authors"])
+        authors = ", ".join(
+            f"{author['first_name'][:1]}. {author['last_name']}"
+            for author, _ in zip(pub_meta["authors"], range(max_authors if n_authors < 20 else 1))
+        )
+        if n_authors > max_authors:
+            authors += " et al."
+        f.write("\n".join([
+                f'- authors: "{authors}"',
+                f'  title: "{title}"',
+                f'  arxiv: "{arxiv_id}"'
+            ] + (
+                [f'  journal: "{journal}"'] if journal is not None else []
+            )
+        ))
+        f.write('\n')
+
